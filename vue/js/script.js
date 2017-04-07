d056fea45b1e1533e024eedd7fe107099737bd0c
giripriya <tumkurgiripriya@gmail.com>
2017-04-07 17:44:57 +0530

    Img added locally to project

--- vue/js/script.js
+++ vue/js/script.js
@@ -5,7 +5,7 @@ var lastVertex = 1;
 var lengthInMeters;
 var delayer;
 var index  = -1;
-var image = ['http://www.iconninja.com/files/682/734/552/delivery-logistics-renault-vehicle-front-truck-icon.png',
+var image = ['img/delivery-logistics-renault-vehicle-front-truck-icon.png',
              'http://www.iconsplace.com/icons/preview/purple/truck-32.png',
              'http://www.iconsplace.com/icons/preview/pink/truck-32.png',
             ]
